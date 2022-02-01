c9631e7f3e82e66a0c321ac763b7af9acc633a45
chanchal-Trantor <chanchal.narang@trantorinc.com>
2022-02-01 15:48:29 +0530

    register form layout ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,RegisterModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/register/register.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,ReactiveFormsModule
+  ],
+  exports:[
+    RegisterComponent
+  ]
+})
+export class RegisterModule { }
